a88f4019589b081490cc114ba0eb4245bd778dcb
bibisixtynine <ilboued@gmail.com>
2023-09-14 00:49:53 +0200

    Text, Circle, HStack, VStack

--- BasicWebPage/main.js
+++ BasicWebPage/main.js
@@ -1,14 +1,5 @@
 // main.js
 
-// add a print to body function
-function printToBody(msg) {
-    var div = document.createElement("div");
-    div.innerHTML = msg;
-    document.body.appendChild(div);
-}
-
-printToBody("Hello World!")
-
 // Define a Text function with background and foreground color modifiers
 function Text(text) {
     const element = document.createElement('div');
@@ -43,7 +34,54 @@ function Text(text) {
         },
     };
 
+    // Center the text horizontally
+    element.style.display = 'flex';
+    element.style.justifyContent = 'center';
+    element.style.alignItems = 'center';
+    element.style.margin = 'auto';
+
     return textObj;
+}
+
+// Define a Circle function
+function Circle(radius) {
+    const circleElement = document.createElement('div');
+    circleElement.style.width = `${2 * radius}px`;
+    circleElement.style.height = `${2 * radius}px`;
+    circleElement.style.borderRadius = '50%';
+    circleElement.style.backgroundColor = 'blue'; // You can set the default color here
+
+    const circleObj = {
+        element: circleElement,
+        backgroundColor: function (color) {
+            if (color) {
+                circleElement.style.backgroundColor = color;
+            }
+            return circleObj;
+        },
+        onClick: function (callback) {
+            if (callback) {
+                circleElement.addEventListener('click', callback);
+                circleElement.style.cursor = 'pointer';
+            }
+            return circleObj;
+        },
+        radius: function (newRadius) {
+            if (newRadius) {
+                circleElement.style.width = `${2 * newRadius}px`;
+                circleElement.style.height = `${2 * newRadius}px`;
+            }
+            return circleObj;
+        },
+    };
+
+    // Center the circle horizontally and vertically
+    circleElement.style.display = 'flex';
+    circleElement.style.justifyContent = 'center';
+    circleElement.style.alignItems = 'center';
+    circleElement.style.margin = 'auto'; // Center the circle horizontally
+
+    return circleObj;
 }
 
 // Define an HStack function
@@ -51,6 +89,7 @@ function HStack(...children) {
     const stackElement = document.createElement('div');
     stackElement.style.display = 'flex';
     stackElement.style.flexDirection = 'row';
+    stackElement.style.justifyContent = 'space-between'; // Equal spacing between children
 
     children.forEach((child) => {
         if (child && child.element) {
@@ -70,6 +109,7 @@ function VStack(...children) {
     const stackElement = document.createElement('div');
     stackElement.style.display = 'flex';
     stackElement.style.flexDirection = 'column';
+    stackElement.style.justifyContent = 'center'; // Center children vertically
 
     children.forEach((child) => {
         if (child && child.element) {
@@ -84,20 +124,81 @@ function VStack(...children) {
     return stackObj;
 }
 
+
+function Spacer() {
+    const spacerElement = document.createElement('div');
+    spacerElement.style.flex = 1; // Use flexGrow to create space
+
+    const spacerObj = {
+        element: spacerElement,
+    };
+
+    return spacerObj;
+}
+
+// Define a Body function
+function Body(...children) {
+    const bodyElement = document.createElement('div');
+    bodyElement.style.display = 'flex';
+    bodyElement.style.flexDirection = 'column';
+    bodyElement.style.justifyContent = 'center'; // Center children vertically
+
+    bodyElement.style.width = '100%';
+    bodyElement.style.height = '100%';
+    bodyElement.style.position = 'fixed';
+    bodyElement.style.top = '0';
+    bodyElement.style.left = '0';
+    children.forEach((child) => {
+        if (child && child.element) {
+            bodyElement.appendChild(child.element);
+        }
+    });
+
+    const bodyObj = {
+        element: bodyElement,
+    };
+
+    document.body.appendChild(bodyObj.element);
+
+    return bodyObj;
+}
+
 // Usage example with HStack and VStack:
 
 
-const vStackView = VStack(
-    Text("Hello World")
-      .onClick(() => alert("Hello World")),
-    Text("I'm Robert").foregroundColor("red"),
-    Text("I'm happy"),
+
+
+Body(
+    Text("My First Web Site")
+        .fontSize("50px"),
+    Text("designed with JavaScriptUI"),
+    Spacer(),
     HStack(
-        Text("Hello World"),
-        Text("I'm Robert"),
-        Text("I'm happy")
-    )
-);
+        Circle(50),
+        Circle(50)
+    ),
+    Spacer(),
+    Text("click me")
+        .onClick(() => alert("Ouch !"))
+        .backgroundColor("blue"),
+    Spacer(),
+    HStack(
+        Spacer(),
+        Text("Hello World")
+            .backgroundColor("red"),
+        Spacer(),
+        Text("I'm Robert").backgroundColor("red"),
+        Spacer(),
+        Text("I'm happy").backgroundColor("red"),
+        Spacer(),
+        Circle(20),
+        Spacer()
+    ),
+    Spacer()
+        
+)
 
 
-document.body.appendChild(vStackView.element);
+
+
+
